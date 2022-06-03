3de5fffed511f0da92fe1442d7b940df4e5ecc9d
Tomasz Bułat <buatss@gmail.com>
2022-06-03 15:55:31 +0200

    correct dependencies, implement isAllPositiveNumbers(...) method

--- core/src/main/java/Utils.java
+++ core/src/main/java/Utils.java
@@ -1,6 +1,18 @@
+import BuildTools.*;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Utils {
     static boolean isAllPositiveNumbers(String... str) {
-        System.out.println("isAllPositiveNumbers called");
-        return true;
+        //full classpath is needed to not mistake with this class
+        BuildTools.Utils utils = new BuildTools.Utils();
+        Optional<Boolean> optional = Arrays
+                .stream(str)
+                .map(val -> utils.isPositiveNumber(val))
+                .filter(bool -> bool.equals(false))
+                .findFirst();
+
+        return !optional.isPresent();
     }
 }
